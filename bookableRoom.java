054a21b0fe595c192b2e5af27621017f0b5198e1
yusufaltattan <73115671+yusufaltattan@users.noreply.github.com>
2021-02-22 17:23:52 +0300

    commit

--- bookableRoom.java
+++ bookableRoom.java
@@ -1,7 +1,7 @@
 public class bookableRoom {
     // Properties
     private universityResources[] rooms;
-    private String time; // Need to implement actual time? or just 3 time slots?
+    private String[] time; // just 3 time slots?
     private int occupancy;
     private int statusRoom; // Not sure whether to implement using 1 = FirstStatus, 2 = Second... or using Enum
      
@@ -9,7 +9,7 @@ public class bookableRoom {
     public universityResources[] getRooms() {
         return this.rooms; // Do I need to do this?
     }
-    public String getTime() {
+    public String[] getTime() {
         return this.time;
     }
     public int getOccupancy() {
@@ -23,7 +23,7 @@ public class bookableRoom {
     public void setRooms(universityResources[] rooms) {
         this.rooms = rooms;
     }
-    public void setTime(String time) {
+    public void setTime(String[] time) {
         this.time = time;
     }
     public void setOccupancy(int occupancy) {
@@ -34,11 +34,13 @@ public class bookableRoom {
     }
 
     // Constructors
-    public bookableRoom(universityResources[] rooms, String time, int occupancy, int statusRoom) {
+    public bookableRoom(universityResources[] rooms, String[] time, int occupancy, int statusRoom) {
         this.rooms = rooms;
         this.time = time;
         this.occupancy = occupancy;
         this.statusRoom = statusRoom;
     }
 
+    // Methods
+  
 }
--- bookings.java
+++ bookings.java
@@ -1,3 +1,5 @@
+import java.util.*; 
+
 public class bookings {
     // Properties
     private bookingsystem[] bookingSystem;
@@ -42,6 +44,36 @@ public class bookings {
     }
 
     public static void main(String[] args) {
+        System.out.println("University of Knowledge - COVID test "+ "\n" + "Manage Bookings" + "\n" + "Please enter the number to select your option:" + "\n" + "To manage Bookable Rooms:" + "\n" + "\t" + "1. List" + "\n" + "\t" + "2. Add" + "\n" + "\t" + "3. Remove" + "\n" + "To manage Assistans on Shift:" + "\n" + "\t" + "4. List" + "\n" + "\t" + "5. Add" + "\n" + "\t" + "6. Remove" + "\n" + "To manage Bookings:" + "\n" + "\t" + "7. List" + "\n" + "\t" + "8. Add" + "\n" + "\t" + "9. Remove" + "\n" + "\t" + " 10. Conclude" + "\n" + "After selecting one the options above, you will be presented other screens." +"\n" + "If you press 0, you will be able to return to this main menu." + "\n" + "Press -1 (or ctrl+c) to quit this application." + "\n");
+        Scanner sc = new Scanner(System.in);
+        String option = sc.nextLine();
+        if (option.equals("1")) {
+            System.out.print("You chose option 1");
+        } 
+        else if (option.equals("2")) {
+            System.out.print("You chose option 2");
+        }
+        else if (option.equals("3")) {
+            System.out.print("You chose option 3");
+        }
+        else if (option.equals("4")) {
+            System.out.print("You chose option 4");
+        }
+        else if (option.equals("5")) {
+            System.out.print("You chose option 5");
+        }
+        else if (option.equals("6")) {
+            System.out.print("You chose option 6");
+        }
+        else if (option.equals("7")) {
+            System.out.print("You chose option 7");
+        }
+        else if (option.equals("8")) {
+            System.out.print("You chose option 8");
+        }
+        else if (option.equals("9")) {
+            System.out.print("You chose option 9");
+        }
 
     }
 }
